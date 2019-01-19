36e5821eb256f3e1ab358cb8c9b1f1d2a54c6ae1
activehack <danny@activehack.com>
2019-01-19 08:48:28 +0800

    Create initial.sql

---
+++ docs/virtualmail/sql/initial.sql
@@ -0,0 +1,42 @@
+CREATE TABLE accounts (
+    id SERIAL,
+    username character varying(32) NOT NULL,
+    domain character varying(64) NOT NULL,
+    label character varying(64) NOT NULL,
+    usage integer NOT NULL,
+    secret character varying(128) NOT NULL,
+    status character varying(16) NOT NULL
+) WITHOUT OIDS;
+
+CREATE TABLE domains (
+    id SERIAL,
+    domain character varying(64) NOT NULL,
+    status character varying(16) NOT NULL
+) WITHOUT OIDS;
+
+CREATE TABLE list_accounts (
+    id SERIAL,
+    name character varying(32) NOT NULL,
+    domain character varying(64) NOT NULL,
+    status character varying(16) NOT NULL
+) WITHOUT OIDS;
+
+CREATE TABLE list_members (
+    id SERIAL,
+    list_account character varying(96) NOT NULL,
+    member_email character varying(96) NOT NULL,
+    status character varying(16) NOT NULL
+) WITHOUT OIDS;
+
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_index0 UNIQUE (username, domain);
+ALTER TABLE ONLY accounts ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY domains ADD CONSTRAINT domains_index0 UNIQUE (domain);
+ALTER TABLE ONLY domains ADD CONSTRAINT domains_pkey PRIMARY KEY (domain);
+ALTER TABLE ONLY list_accounts ADD CONSTRAINT list_accounts_index0 UNIQUE (name, domain);
+ALTER TABLE ONLY list_accounts ADD CONSTRAINT list_accounts_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY list_members ADD CONSTRAINT list_members_index0 UNIQUE (member_email);
+ALTER TABLE ONLY list_members ADD CONSTRAINT list_members_key PRIMARY KEY (id);
+CREATE INDEX domains_index1 ON domains USING btree (status);
+CREATE INDEX domains_index2 ON domains USING btree (domain, status);
+CREATE INDEX list_account_index1 ON list_accounts USING btree (status, name, domain);
+CREATE INDEX list_member_index1 ON list_members USING btree (list_account, status);
